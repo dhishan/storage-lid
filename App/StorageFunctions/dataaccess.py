cc2b1f11cb9f353f292d266d7f6ff68fb756e00b
Dhishan Amaranath <iamdhishan@gmail.com>
2021-03-10 18:27:17 -0500

    compiler error

--- App/StorageFunctions/dataaccess.py
+++ App/StorageFunctions/dataaccess.py
@@ -2,9 +2,7 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 import os
 
-# os.environ["AZURE_TENANT_ID"] = "94a8b28b-7de6-4eba-af01-5dfd2c03c072"
-# os.environ["AZURE_CLIENT_ID"] = "d949f8e0-b680-42b2-a3df-51345bef083a"
-# token_credential = InteractiveBrowserCredential()
+
 
 storage_account_url = "https://datastrdhishan.blob.core.windows.net/"
 token_credential = DefaultAzureCredential()
@@ -17,6 +15,6 @@ def get_user_object(name):
     container_client = blob_service_client.get_container_client("users")
     blob_client = container_client.get_blob_client(name)
     download_data = blob_client.download_blob()
-    return content_as_text(max_concurrency=1, encoding='UTF-8')
+    return download_data.content_as_text(max_concurrency=1, encoding='UTF-8')
 
-# print(get_user_object("user1.json"))
+print(get_user_object("user1.json"))
