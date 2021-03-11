c2dfb9c92441d8772c7bbaa54f7874374285fc94
Dhishan Amaranath <iamdhishan@gmail.com>
2021-03-11 18:44:43 -0500

    Webpage Serving REST API

--- App/app.py
+++ App/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
-from StorageFunctions.dataaccess import *
-app = Flask(__name__)
+from flask import Flask, render_template
+import connexion
+
+# app = Flask(__name__)
+
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
 
 @app.route("/")
 def hello():
     # print(type(get_user_object("user1.json")))
-    return get_user_object("user1.json")
+    # return "Welcome to API"
+    return render_template('home.html')
+    # return redirect(url_for('/api/ui'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
